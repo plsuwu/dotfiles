517c4f888a17d026db4b90832ba8a8f61888eb8d
pls <zac.womal@outlook.com>
2024-02-16 04:19:19 +1000

    Update copy_conf.py
    some comments and stufdf

--- copy_conf.py
+++ copy_conf.py
@@ -2,17 +2,32 @@
 
 import subprocess
 import shlex
+import os
 
-CONF_DIR="/home/please/.config/"
+# typing & os module changes are not tested i did these on a whim!!!
+HOME=os.getenv("HOME")
+CONF_DIR=f"{HOME}/.config/"
 
-# "repository_dir" : "local_items"
-target_items = {
+# "repository_dir" : ["path/to/local_item_one", "path/to/local_item_two"]
+target_items: dict[str,list[str]] = {
         "alacritty": [
             "alacritty/alacritty.toml"
-            ],
-
-        # idk how to do globbing in subprocess but its
-        # not worth googling rn ("eww": "eww/*")
+        ],
+        
+        # i can't remember how to insert items into a nested dict list; 
+        # but this seems like it would work, right?.
+        
+        # instead of globbing, we could instead use something like:
+        # ```
+        # ... = { 
+        #         "eww": [],
+        # ...
+        # }
+        # 
+        # eww_dir: list[str] = os.listdir(f"{CONF_DIR}/eww")
+        # for i in eww_dir:
+        #        target_items["eww"].append(f"{CONF_DIR}/{i}")
+        # ```
         "eww": [
             "eww/palette/",
             "eww/scripts/",
@@ -21,16 +36,16 @@ target_items = {
             "eww/eww.yuck",
             "eww/tests.yuck",
             "eww/variables.yuck"
-            ],
+        ],
         "feh": [
             "feh/main.png"
-            ],
+        ],
         "gtk-3.0": [
             "gtk-3.0/settings.ini"
-            ],
+        ],
         "neofetch": [
             "neofetch/config.conf"
-            ],
+        ],
         "nvim": [
             "nvim/lua/",
             "nvim/init.lua"
